94847626a25019bfccb9b8b5a40e7ac23d8ce139
Marián Hlaváč <hlavac95@gmail.com>
2017-11-16 16:43:42 +0100

    Fix errorous compile script

--- compile.sh
+++ compile.sh
@@ -3,11 +3,14 @@
 if [ "$ENV_STAR" = "1" ]
 then
   echo "Compiling binaries for star cluster..."
-  pgc++ -acc -ta=nvidia,time -Minfo=accel -o bin/fft-cuda src/fft-cuda.cpp
+  pgc++ -acc -ta=nvidia,time -Minfo=accel -o bin/fft-cuda-acc src/fft-cuda.cpp
+else
+  echo "Compiling tool for creating data..."
+  g++ -o bin/create-data src/create-data.cpp 
 fi
 
-echo "Compiling binaries..."
+echo "Compiling FFT-cuda binaries..."
 g++ -o bin/fft-cuda src/fft-cuda.cpp 
-g++ -o bin/create-data src/create-data.cpp 
+
 
 echo "Compiled."
