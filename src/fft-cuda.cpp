41c74391b69d61837aaf71fa37c581ddab7e3f01
Marián Hlaváč <hlavac95@gmail.com>
2017-11-19 11:59:33 +0100

    Replace the recursive algorithm with iterative

--- src/fft-cuda.cpp
+++ src/fft-cuda.cpp
@@ -11,23 +11,39 @@ typedef complex<double> Complex;
 typedef valarray<Complex> CplxArray;
 
 /**
- * Recursive, in-place Fast-Fourier Transformation.
- * https://rosettacode.org/wiki/Fast_Fourier_transform#C.2B.2B
+ * In-place Iterative Fast Fourier Transformation.
  */
-void fft(CplxArray& x) {
-    const size_t N = x.size();
-    if (N <= 1) return;
- 
-    CplxArray even = x[slice(0, N/2, 2)];
-    CplxArray odd = x[slice(1, N/2, 2)];
- 
-    fft(even); fft(odd);
- 
-    for (size_t k = 0; k < N/2; ++k) {
-        Complex t = polar(1.0, -2 * M_PI * k / N) * odd[k];
-        x[k] = even[k] + t;
-        x[k+N/2] = even[k] - t;
-    }
+CplxArray fft(CplxArray& x) {
+    const size_t n = x.size();
+    CplxArray r(n);
+  	int s = log2(n);
+  	
+    // Bit-reversal reordering
+  	for (int i = 0; i <= n-1; i++) {
+  		int j = i, k = 0;
+      
+  		for (int l = 1; l <= s; l++) {
+  			k = k * 2 + (j & 1);
+        j >>= 1;
+      }
+
+  		r[k] = x[i];
+  	}
+    
+  	for (int i = 1; i <= s; i++) {
+  		int m = 1 << i;
+      
+  		for (int j = 0; j < n; j += m) for(int k = 0; k < m/2; k++) {
+        Complex t = polar(1.0, -2 * M_PI * k / m);
+  			Complex u = r[j + k];
+  			t *= r[j + k + m / 2];
+
+  			r[j + k] = u + t; 
+  			r[j + k + m / 2] = u - t;
+  		}
+  	}
+    
+  	return r;
 }
 
 /**
@@ -53,8 +69,9 @@ void read_file(char* filename, vector<double>& out) {
   if (file.is_open()) {
     while (!file.eof()) {
       double val;
-      file >> val;
-      out.push_back(val);
+      if (file >> val) {
+        out.push_back(val);
+      }
     }
   }
   
@@ -75,12 +92,21 @@ int main(int argc, char** argv) {
   int count = buffer.size();
   CplxArray data = *real_to_complex(buffer);
   
+  // Is power of 2?
+  if (count & (count-1)) {
+    cerr << "Input data sample count have to be power of two." << endl;
+    return 2;
+  }
+  
+  // Print out stats
+  cout << "Data info:" << endl << "Sample count: " << count << endl << "Sample rate: " << sample_rate << endl;
+  
   // Start the stopwatch
   clock_t t;
 	t = clock();
   
   // Run FFT algorithm with loaded data
-  fft(data);
+  CplxArray result = fft(data);
   
   // Print out the total elapsed time
   t = clock() - t;
@@ -92,7 +118,7 @@ int main(int argc, char** argv) {
   outfile.precision(4);
   outfile << "frequency, value" << endl;
   for (int i = 0; i < count / 2; i++) {
-      outfile << i * ((double)sample_rate/count) << "," << abs(data[i]) << endl;
+      outfile << i * ((double)sample_rate/count) << "," << abs(result[i]) << endl;
   }
   outfile.close();
   
