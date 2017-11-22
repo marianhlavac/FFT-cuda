62223f34e0886494407389519b02df7737ff59ea
Marián Hlaváč <hlavac95@gmail.com>
2017-11-22 16:31:01 +0100

    Change the compile script to reflect new changes

--- compile.sh
+++ compile.sh
@@ -3,12 +3,10 @@
 if [ "$ENV_STAR" = "1" ]
 then
   echo "Compiling binaries for star cluster..."
-  cp src/fft-cuda.cpp src/fft-cuda-acc.cpp
-  echo -e "#define STAR\n$(cat src/fft-cuda-acc.cpp)" > src/fft-cuda-acc.cpp
-  pgc++ -acc -ta=nvidia:managed,time -Minfo=accel -o bin/fft-cuda-acc src/fft-cuda-acc.cpp
+  pgc++ -acc -fast -Minfo=accel -o bin/fft-cuda-acc -std=c++11 src/fft-cuda.cpp
 else
   echo "Compiling tool for creating data..."
-  g++ -o bin/create-data src/create-data.cpp 
+  g++ -o bin/create-data -std=c++11 src/create-data.cpp 
 fi
 
 if [ "$ENV_LOCAL" = "1" ]
@@ -17,7 +15,7 @@ then
   /opt/pgi/osx86-64/2017/bin/pgc++ -ta=multicore -fast -Minfo=all,ccff -o bin/fft-cuda-acc src/fft-cuda.cpp
 else
   echo "Compiling FFT-cuda binaries..."
-  g++ -o bin/fft-cuda src/fft-cuda.cpp -g
+  g++ -o bin/fft-cuda -std=c++11 src/fft-cuda.cpp -g
 fi
 
 echo "Compiled."
