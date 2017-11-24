7dbb7a92b35f42e8f90c7dc3f2d2d975fddeef6c
Marián Hlaváč <hlavac95@gmail.com>
2017-11-24 14:54:13 +0100

    Update the program to support batch execution

--- src/fft-cuda.cpp
+++ src/fft-cuda.cpp
@@ -1,14 +1,18 @@
-#include <complex>
 #include <iostream>
 #include <fstream>
-#include <valarray>
+#include <sstream>
 #include <chrono>
 #include <vector>
- 
+#include <cmath>
+#include <dirent.h>
+
 using namespace std;
 
+#define NUM_GANGS_START 2
+#define NUM_GANGS_END 512
+
 /**
- * In-place Iterative Fast Fourier Transformation.
+ * In-place Iterative Fast Fourier Transformation with OpenACC support.
  */
 template<int T>
 float* fft(float* __restrict__ x, size_t n) {
@@ -64,6 +68,8 @@ float* fft(float* __restrict__ x, size_t n) {
       
             r[ridx2] = u[0] - t[0];
       			r[ridx2 + 1] = u[1] - t[1];
+            
+            // Whew. That was ugly. Eww.
           }
     		}
         
@@ -76,23 +82,9 @@ float* fft(float* __restrict__ x, size_t n) {
 }
 
 /**
- * Converts array of real numbers to complex numbers array.
- */
-// double** real_to_complex(vector<double> real_array) {
-//   double** ary = new double*[real_array.size()];
-//   for (int i = 0; i < real_array.size(); i++) {
-//       ary[i] = new double[2];
-//       ary[i][0] = real_array[i];
-//       ary[i][1] = 0.0;
-//   }
-// 
-//   return ary;
-// }
-
-/**
  * Reads numeric data from a file.
  */
-void read_file(char* filename, vector<float>& out) {
+void read_file(const char* filename, vector<float>& out) {
   ifstream file;
   file.open(filename);
   
@@ -103,47 +95,34 @@ void read_file(char* filename, vector<float>& out) {
         out.push_back(val);
       }
     }
+  } else {
+    cerr << "Can't open file " << filename << " for reading." << endl;
   }
   
   file.close();
 }
 
-int main(int argc, char** argv) {
-  srand (time(NULL));
-  
-  // Deal with program arguments
-  if (argc < 3) {
-    cerr << "Usage: " << argv[0] << " [input_file] [output_file] [sample_rate]"; return 2;
-  }
-  
-  vector<float> buffer;
-  int sample_rate = atoi(argv[3]); 
-  
-  // Read data file
-  read_file(argv[1], buffer);
-  int count = buffer.size();
-  
-  // Is power of 2?
-  if (count & (count-1)) {
-    cerr << "Input data sample count have to be power of two." << endl;
-    return 2;
-  }
-  
-  // Print out stats
-  cout << "Data info:" << endl << "Sample count: " << count << endl << "Sample rate: " << sample_rate << endl;
-  
-  // Go through initialization
-  fft<1>(&buffer[0], count);
-  
+void compute(int num_gangs, float* buffer, size_t count, int sample_rate, const char* filename) {
   float time = 0;
+  
+  float* result = new float[1];
   
   for (int i = 0; i < 4; i++) {
     // Start the stopwatch
     auto start = chrono::high_resolution_clock::now();
     
     // Run FFT algorithm with loaded data
-    float* result = fft<32>(&buffer[0], count);
-    
+    switch (num_gangs) {
+      case 2: result = fft<2>(buffer, count); break;
+      case 4: result = fft<4>(buffer, count); break;
+      case 8: result = fft<8>(buffer, count); break;
+      case 16: result = fft<16>(buffer, count); break;
+      case 32: result = fft<32>(buffer, count); break;
+      case 64: result = fft<64>(buffer, count); break;
+      case 128: result = fft<128>(buffer, count); break;
+      case 256: result = fft<256>(buffer, count); break;
+      case 512: result = fft<512>(buffer, count); break;
+    }
     // Log the elapsed time
     auto finish = chrono::high_resolution_clock::now();
     auto microseconds = chrono::duration_cast<std::chrono::microseconds>(finish-start);
@@ -151,17 +130,74 @@ int main(int argc, char** argv) {
     time += microseconds.count();
   }
   
-  cout << "For num_gangs(32) program took " << time/4 << " us to finish." << endl;
+  cout << count << "," << sample_rate << "," << num_gangs << "," << time/4 << endl;
   
-  // // Save the computed data
-  // ofstream outfile;
-  // outfile.open (argv[2]);
-  // outfile.precision(4);
-  // outfile << "frequency, value" << endl;
-  // for (int i = 0; i < count / 2; i++) {
-  //     outfile << i * ((float)sample_rate/count) << "," << result[i] << endl;
-  // }
-  // outfile.close();
+  // Save the computed data
+  char* outfilename = new char[512];
+  strcpy(outfilename, filename);
+  strcat(outfilename, ".out");
+  ofstream outfile;
+  outfile.open (outfilename);
+  outfile.precision(4);
+  outfile << "frequency, value" << endl;
+  for (int i = 0; i < count / 2; i++) {
+      outfile << i * ((float)sample_rate/count) << "," << result[i] << endl;
+  }
+  outfile.close();
+}
+
+void compute_file(const char* folder, const char* filename, 
+                  const char* sample_count, int sample_rate) {
+  vector<float> buffer;
+  
+  // Read data file
+  read_file(filename, buffer);
+  int count = buffer.size();
+  
+  // Is power of 2?
+  if (count & (count-1)) {
+    cerr << "Input data sample count have to be power of two." << endl;
+  } else {
+    // Go through initialization
+    fft<1>(&buffer[0], count);
+    
+    // Run compute for various num_gangs
+    for (int ng = NUM_GANGS_START; ng <= NUM_GANGS_END; ng <<= 1) {
+      cout << folder << ",";
+      compute(ng, &buffer[0], count, sample_rate, filename);
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  srand (time(NULL));
+  
+  // Deal with program arguments
+  if (argc < 2) {
+    cerr << "Usage: " << argv[0] << " [input_folder]"; return 2;
+  }
+
+  // Compute all files in folder
+  DIR* dirp = opendir(argv[1]);
+  struct dirent *epdf;
+  while ((epdf = readdir(dirp)) != NULL) {
+    size_t len = strlen(epdf->d_name);
+    if (strcmp(epdf->d_name,".") != 0 && strcmp(epdf->d_name,"..") != 0
+        && strcmp(&epdf->d_name[len-3], "dat") == 0) {
+      stringstream fname(epdf->d_name);
+      string samples, sr;
+      
+      getline(fname, samples, '@');
+      getline(fname, sr, '.');
+      
+      char* fold = new char[512];
+      strcpy(fold, argv[1]);
+      
+      compute_file(argv[1], strcat(strcat(fold, "/"), epdf->d_name), samples.c_str(), atoi(sr.c_str()));
+    }
+  }
+    
+  closedir(dirp);
   
   return 0;
 }
